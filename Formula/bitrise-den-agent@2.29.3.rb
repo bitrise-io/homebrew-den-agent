c2c439ab5e2d18a51e5c4af087031491681ae6f1
nagypeterjob <peter.nagy@bitrise.io>
2024-03-05 11:26:24 +0100

    fix: syntax

--- Formula/bitrise-den-agent@2.29.3.rb
+++ Formula/bitrise-den-agent@2.29.3.rb
@@ -14,12 +14,14 @@ class BitriseDenAgentAT2293 < Formula
       if Hardware::CPU.intel?
         url "https://github.com/bitrise-io/bitrise-den-agent/releases/download/v2.29.3/bitrise-den-agent-darwin-amd64.zip"
         sha256 "7dad2dd6c05114770dbb838cc48341da368519617fdb14abd1f3226e84a3b75d"
+      end
     end
   
     on_linux do
       if Hardware::CPU.intel?
         url "https://github.com/bitrise-io/bitrise-den-agent/releases/download/v2.29.3/bitrise-den-agent-linux-amd64.zip"
         sha256 "91e14450d676e481e1a12f3b167a9538940727fcbc78a5c832f71022cf6487d8"
+      end
     end
   
     def install
